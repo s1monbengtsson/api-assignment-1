772e527aa5a6e1b29622255d66f6d9e0a8f6ecf9
Simon Bengtsson <dev.simon.bengtsson@gmail.com>
2023-01-31 23:20:06 +0100

    set up controllers

---
+++ src/controllers/order_controller.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express'
+import { validationResult } from 'express-validator'
+import prisma from '../prisma'
+
+// get all resources
+export const index = async (req: Request, res: Response) => {
+}
+
+// get a single resource
+export const show = async (req: Request, res: Response) => {
+}
+
+// creates a new resource
+export const store = async (req: Request, res: Response) => {
+}
