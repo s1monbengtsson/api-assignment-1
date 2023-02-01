1ae8963db53a9f8f22dd537d255546cc40dbfb04
Simon Bengtsson <dev.simon.bengtsson@gmail.com>
2023-02-01 14:21:01 +0100

    added endpoints for router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import express from "express"
-import resource from './_router'
+import products from './products'
+import orders from './orders'
 
 // instantiate a new router
 const router = express.Router()
@@ -8,14 +9,13 @@ const router = express.Router()
  * GET /
  */
 router.get('/', (req, res) => {
-	res.send({
-		message: "I AM API, BEEP BOOP",
-	})
+    res.send({
+        message: "I AM API, BEEP BOOP",
+    })
 })
 
-/**
- * [EXAMPLE] /resource
- */
-// router.use('/resource', resource)
+router.use('/products', products)
+
+router.use('/orders', orders)
 
 export default router
