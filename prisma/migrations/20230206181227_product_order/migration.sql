3743998d1f2831962dd4b9c8831d26068b7a1f78
sb <dev.simon.bengtsson@gmail.com>
2023-02-06 19:12:47 +0100

    removed old and added new prisma migrations

--- prisma/migrations/20230206181227_product_order/migration.sql
+++ prisma/migrations/20230206181227_product_order/migration.sql
@@ -14,6 +14,7 @@ CREATE TABLE `Product` (
 -- CreateTable
 CREATE TABLE `Order` (
     `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    `order_date` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `customer_first_name` VARCHAR(191) NOT NULL,
     `customer_last_name` VARCHAR(191) NOT NULL,
     `customer_address` VARCHAR(191) NOT NULL,
@@ -22,6 +23,8 @@ CREATE TABLE `Order` (
     `customer_email` VARCHAR(191) NOT NULL,
     `customer_phone` VARCHAR(191) NULL,
     `order_total` INTEGER UNSIGNED NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -29,17 +32,17 @@ CREATE TABLE `Order` (
 -- CreateTable
 CREATE TABLE `OrderItems` (
     `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    `order_id` INTEGER UNSIGNED NOT NULL,
     `qty` INTEGER UNSIGNED NOT NULL,
+    `product_id` INTEGER UNSIGNED NOT NULL,
     `item_price` INTEGER UNSIGNED NOT NULL,
     `item_total` INTEGER UNSIGNED NOT NULL,
-    `productId` INTEGER UNSIGNED NOT NULL,
-    `orderId` INTEGER UNSIGNED NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `OrderItems` ADD CONSTRAINT `OrderItems_productId_fkey` FOREIGN KEY (`productId`) REFERENCES `Product`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `OrderItems` ADD CONSTRAINT `OrderItems_product_id_fkey` FOREIGN KEY (`product_id`) REFERENCES `Product`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `OrderItems` ADD CONSTRAINT `OrderItems_orderId_fkey` FOREIGN KEY (`orderId`) REFERENCES `Order`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `OrderItems` ADD CONSTRAINT `OrderItems_order_id_fkey` FOREIGN KEY (`order_id`) REFERENCES `Order`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
