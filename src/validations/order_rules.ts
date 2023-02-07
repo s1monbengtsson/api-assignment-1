6e1105b7a1218f41677bb5acbb92b14d3a05e36f
sb <dev.simon.bengtsson@gmail.com>
2023-02-07 20:13:22 +0100

    set postcode to only accept 5 digits

--- src/validations/order_rules.ts
+++ src/validations/order_rules.ts
@@ -8,7 +8,7 @@ export const orderRules = [
     body('customer_first_name').isString().withMessage('Has to be a string').bail().isLength({ min: 2 }).withMessage('Has to be minimum 2 characters long'),
     body('customer_last_name').isString().withMessage('Has to be a string').bail().isLength({ min: 2 }).withMessage('Has to be minimum 2 characters long'),
     body('customer_address').isString().withMessage('Has to be a string').bail().isLength({ min: 3 }).withMessage('Has to be minimum 3 characters long'),
-    body('customer_postcode').isString().withMessage('Has to be a string').bail().isLength({ min: 5, max: 6 }).withMessage('Has to be between 5 and 6 characters long'),
+    body('customer_postcode').isString().withMessage('Has to be a string').bail().isLength({ min: 5, max: 5 }).withMessage('Has to be between 5 characters long'),
     body('customer_city').isString().withMessage('Has to be a string').bail().isLength({ min: 2 }).withMessage('Has to be minimum 2 characters long'),
     body('customer_email').isEmail().withMessage('Has to be a valid email'),
     body('customer_phone')?.isString().withMessage('Has to be a string').bail().isLength({ min: 10, max: 12 }).withMessage('Has to be between 10 and 12 characters long'),
@@ -17,4 +17,3 @@ export const orderRules = [
     body('order_items.*.item_price').isInt({min:1}).withMessage('Cannot be 0'),
     body('order_items.*.item_total').isInt({min:1}).withMessage('Cannot be 0'),
 ]
-
