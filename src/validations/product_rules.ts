0f402a2b95c16a0c234766111d5002c7e316ddf6
sb <dev.simon.bengtsson@gmail.com>
2023-02-07 20:13:03 +0100

    added validation rules for price and stock_status

--- src/validations/product_rules.ts
+++ src/validations/product_rules.ts
@@ -7,8 +7,8 @@ import { body } from 'express-validator'
 export const productRules = [
     body('name').isString().withMessage('Has to be a string').bail().isLength({ min: 3 }).withMessage('Has to be minimum 2 characters long'),
     body('description').isString().withMessage('Has to be a string'),
-    body('price').isNumeric().withMessage('Has to be a number').bail().notEmpty(),
+    body('price').isInt({min: 1}).withMessage('Has to be a number larger than 0'),
     body('images').isObject().withMessage('Has to be an object'),
-    body('stock_status').isString().withMessage('Has to be a string'),
-    body('stock_quantity').isNumeric().withMessage('Has to be a number'),
+    body('stock_status').isIn(["instock", "outofstock"]).withMessage('Has to be a string of "instock" or "outofstock"'),
+    body('stock_quantity').isInt({min: 0}),
 ]
