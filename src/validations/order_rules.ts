fc605dc4999a65f2ee3b2dfca569f6330aa7d5e4
sb <dev.simon.bengtsson@gmail.com>
2023-02-08 23:03:27 +0100

    adjusted validation for phone numbers so it matched validation on frontend.

--- src/validations/order_rules.ts
+++ src/validations/order_rules.ts
@@ -11,7 +11,7 @@ export const orderRules = [
     body('customer_postcode').isString().withMessage('Has to be a string').bail().isLength({ min: 5, max: 6 }).withMessage('Has to be between 5 characters long'),
     body('customer_city').isString().withMessage('Has to be a string').bail().isLength({ min: 2 }).withMessage('Has to be minimum 2 characters long'),
     body('customer_email').isEmail().withMessage('Has to be a valid email'),
-    body('customer_phone')?.isString().withMessage('Has to be a string').bail().isLength({ min: 10, max: 10}).withMessage('Has to be between 10 and 12 characters long'),
+    body('customer_phone').optional().isString().withMessage('Has to be a string').bail().isLength({ min: 10, max: 10}).withMessage('Has to be 10 characters long'),
     body('order_items.*.product_id').isInt({min:1}).withMessage('Cannot be 0'),
     body('order_items.*.qty').isInt({min:1}).withMessage('Cannot be 0'),
     body('order_items.*.item_price').isInt({min:1}).withMessage('Cannot be 0'),
